1ba05e29b7bcd279a8ea036910a8a5eddc84a6f2
mpgxc <mpgx5.c@gmail.com>
2021-06-09 05:33:02 -0300

    fix: change responsabilities to deletes to postgress

--- src/modules/accounts/useCases/DeleteAccount/DeleteAccount.ts
+++ src/modules/accounts/useCases/DeleteAccount/DeleteAccount.ts
@@ -1,17 +1,12 @@
 import { inject, injectable } from 'tsyringe';
 
 import { IUsersRepository } from '@modules/accounts/repositories';
-import { DeleteJobWork } from '@modules/jobWorks/useCases/DeleteJobWork';
-import { RemoveFavoriteJob } from '@modules/jobWorks/useCases/RemoveFavoriteJob';
 
 @injectable()
 class DeleteAccount {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
-
-    private deleteJobWork: DeleteJobWork,
-    private removeFavoriteJob: RemoveFavoriteJob,
   ) {}
 
   async execute(user_id: string): Promise<void> {
@@ -20,22 +15,6 @@ class DeleteAccount {
     if (!user) {
       throw new Error('User does not exists!');
     }
-
-    // user.favorites_jobs.forEach(async element => {
-    //   await this.removeFavoriteJob.execute({ job_id: element.id, user_id });
-    // });
-
-    // user.jobs.forEach(async element => {
-    //   await this.deleteJobWork.execute({ job_id: element.id, user_id });
-    // });
-
-    // user.jobs = [];
-    // user.favorites_jobs = [];
-
-    user.category = undefined;
-    user.city = undefined;
-
-    await this.usersRepository.save(user);
 
     await this.usersRepository.delete(user_id);
   }
