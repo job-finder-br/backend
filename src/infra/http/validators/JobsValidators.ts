940ac6e0cfdfd60f066c557f1a779825d8509c3f
mpgxc <mpgx5.c@gmail.com>
2021-06-22 22:41:43 -0300

    fixes: diable required fields

--- src/infra/http/validators/JobsValidators.ts
+++ src/infra/http/validators/JobsValidators.ts
@@ -3,13 +3,13 @@ import { Joi, Segments } from 'celebrate';
 const JobsValidators = {
   BODY: {
     [Segments.BODY]: Joi.object().keys({
-      remuneration_value: Joi.number().required(),
+      remuneration_value: Joi.number(),
       category_id: Joi.string().uuid().required(),
       email: Joi.string().email().required(),
       phone_number: Joi.string().required(),
       description: Joi.string().required(),
       state_name: Joi.string().required(),
-      represents: Joi.string().required(),
+      represents: Joi.string(),
       city_name: Joi.string().required(),
       title: Joi.string().required(),
       type: Joi.string().required(),
